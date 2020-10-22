e75478fa3765be44bdfa7bc731362800cbd7790a
Sin93 <zinovev.maxim93@mail.ru>
2020-10-22 21:39:30 +0300

    в работе. запрос данных с сервера

--- laboratory/nomenclature/urls.py
+++ laboratory/nomenclature/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('edit/<int:pk>', views.services_edit, name='edit'),
     path('get_nomenclature_data', views.json_nomenclature, name='get_nomenclature_data'),
+    path('get_data', views.json_data, name='get_data'),
 ]
